495ddf0f3109ec08cd79f7591e1ce51e9e2c42cd
BoryaBugatti <149894414+sckrich@users.noreply.github.com>
2025-05-30 17:22:45 +0300

    Frontend added

---
+++ src/components/Header.vue
@@ -0,0 +1,125 @@
+<script setup>
+</script>
+<template>
+<div class="header">
+        <div class="logo-block">
+            <img src="../../public/logo-files/logo-blue.png" alt="logo">
+            <h1>ProMan</h1>
+        </div>
+        <ul>
+            <li>
+            <router-link to="/" href="#">
+              <div class="btn">
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                            <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                          </svg>
+              <p class = "btn-txt">Главная</p>
+              </div></router-link>
+          </li><li>
+            <router-link to="/make-app">
+              <div class="btn">
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+                <polyline points="22,6 12,13 2,6"></polyline>
+              </svg>
+              <p class = "btn-txt">Создать заявку</p>
+              </div></router-link>
+          </li><li>
+            <router-link to="/profile">
+              <div class="btn">
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                <circle cx="12" cy="7" r="4"></circle>
+              </svg>
+              <p class = "btn-txt">Профиль</p>
+              </div></router-link>
+          </li>
+        </ul>
+    </div>
+    <hr>
+</template>
+<style scoped>
+.btn{
+  display: flex;
+  align-items: center;
+  height: 20px;
+}
+svg{
+  margin-right: 4px;
+}
+hr{
+  border: 1px solid black;
+  opacity: 0.1;
+  width: 90vw;
+  margin-left: auto;
+  margin-right: auto;
+}
+a:hover{
+  color: rgb(0, 135, 219);
+  outline: 1px solid rgb(0, 135, 219);
+  transition: 0.1s ease;
+}
+a{
+  display: flex;
+  align-items: center;
+  font-family: "Noto Sans", sans-serif;
+  font-weight: 500;
+  font-size: 14px;
+  text-decoration: none;
+  color: rgb(43, 43, 43);
+  padding: 6px;
+  border-radius: 16px;
+}
+img{
+  width: 40px;
+}
+h3{
+  font-family: "Noto Sans", sans-serif;
+  font-weight: 500;
+  font-size: 12px;
+  color: rgb(43, 43, 43);
+}
+h1{
+  font-family: "Noto Sans", sans-serif;
+  font-weight: 800;
+  color: rgb(0, 135, 219);
+}
+
+.logo-block{
+  display: flex;
+  align-items: center;
+}
+.header{
+  margin-top: 10px;
+  height: 50px;
+  width: 95vw;
+  margin-left: auto;
+  margin-right: auto;
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+}
+ul{
+  align-items: center;
+  list-style: none;
+  display: flex;
+  gap: 8px;
+}
+@media screen and (max-width: 480px){
+	.btn-txt{
+    display: none;
+  }
+  svg{
+    height: 25px;
+    width: 25px;
+    margin: 0;
+  }
+  ul{
+    align-items: center;
+    list-style: none;
+    display: flex;
+    gap: 20px;
+  }
+}
+</style>
---
+++ src/views/Profile.vue
@@ -0,0 +1,157 @@
+<script setup>
+import {ref} from 'vue'
+const username = ref('Данила')
+const useremail = ref('sinukovdan@mail.ru')
+</script>
+<template>
+<div class="profile-wrapper">
+    
+    <div class="wrapper">
+        <h1>Добро пожаловать, {{ username }}!</h1>
+        <div class="user-info-block">
+            <div class="user-avatar">
+            <img class = "avatar" src="https://rossoshanskij-r20.gosweb.gosuslugi.ru/netcat_files/9/260/2186_logo.jpg" alt="">
+        </div>
+            <div class="info">
+                <div class="email-block">
+                    <h3>Ваш E-mal:</h3>
+                    <p class = "email">{{ useremail }}</p>
+                </div>
+                <button type="button">Выйти</button>
+            </div>
+            
+        </div>
+        
+        <h1> Ваши проекты в разработке:</h1>
+        <div class="projects-block">
+            <div class="project">
+                <div class="proj-field">
+                    <p class = "proj-text">Название проекта</p>
+                    <p class = "proj-text">проект 1</p>
+                </div>
+                <hr>
+                <div class="proj-field">
+                    <p class = "proj-text">Стек проекта</p>
+                    <p class = "proj-text">Python, Vue js</p>
+                </div>
+                <hr>
+                <div class="proj-field">
+                    <p class = "proj-text">Дата старта проекта</p>
+                    <p class = "proj-text">22.03.2004</p>
+                </div>
+                <hr>
+                <div class="proj-field">
+                    <p class = "proj-text">Дата окончания</p>
+                    <p class = "proj-text">22.04.2005</p>
+                </div>
+                <hr>
+                <div class="proj-field">
+                    <p class = "proj-text">Направление</p>
+                    <p class = "proj-text">Десктоп</p>
+                </div>
+                <hr>
+                <div class="proj-field">
+                    <p class = "proj-text">Прогресс</p>
+                    <p class = "proj-text-progress">10%</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+<style scoped>
+.proj-text-progress{
+    max-width: 150px;
+    margin-right: 20px;
+    margin-left: 20px;
+    height: 20px;
+    color: rgb(37, 158, 37);
+}
+.proj-field{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.proj-text{
+    max-width: 150px;
+    margin-right: 20px;
+    margin-left: 20px;
+    height: 20px;
+}
+.project{
+    display: flex;
+    width: 90%;
+    border: 1px solid black;
+    padding: 2px;
+    border-radius: 32px;
+}
+.email-block{
+    margin-top: 15px;
+    display: flex;
+    align-items: center;
+    height: 20px;
+}
+button{
+    margin-top: 10px;
+
+    background-color: #ff6e6e;
+    color: white;
+    padding: 6px;
+    border-radius: 16px;
+    border: none;
+}
+button:hover{
+    transition: 0.1s ease;
+    color: #ff5a5a;
+    background-color: white;
+    outline: 1px solid #ff5a5a;
+    cursor: pointer;
+}
+button:active{
+    transform: scale(1.02);
+}
+.profile-wrapper{
+    display: flex;
+    flex-direction: column;
+}
+.avatar{
+    width: 80px;
+    border-radius: 50px;
+    border: 2px solid rgb(0, 135, 219);;
+}
+h1{
+  font-family: "Noto Sans", sans-serif;
+  font-weight: 700;
+  font-size: 24px;
+  margin-left: auto;
+  margin-right: auto;
+  color: rgb(43, 43, 43);
+}
+.email{
+    margin-left: 10px;
+}
+.info{
+    height: 30px;
+}
+h3{
+    font-family: "Noto Sans", sans-serif;
+  font-weight: 700;
+  font-size: 16px;
+  color: rgb(43, 43, 43);
+}
+p{
+    font-family: "Noto Sans", sans-serif;
+  font-weight: 500;
+  font-size: 14px;
+  color: rgb(43, 43, 43);
+}
+.user-info-block{
+    display: flex;
+    margin-left: auto;
+    margin-right: auto;
+    gap: 20px;
+}
+.wrapper{
+    margin-left: 5%;
+}
+</style>
